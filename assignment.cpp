a9a9a0250a951ed61cd93e7b9689a23be5d867f6
Quench <clexllama@gmail.com>
2025-12-01 01:41:35 +0800

    Update assignment.cpp

--- assignment.cpp
+++ assignment.cpp
@@ -1,29 +1,95 @@
 #include <iostream>
+#include <string>
 using namespace std;
-class store
+
+struct Item
+{
+    int id;
+    string name;
+    float price;
+    int quantity;
+};
+
+class Store
 {
 private:
+
     /* data */
+
 public:
-    store(/* args */);
-    ~store();
+    void addItem();
+    void viewItems();
+    void editItem();
+    void removeItem();
+    void searchItem();
 };
 
-store::store(/* args */)
-{
+void Store::addItem(){
+
 }
 
-store::~store()
-{
+void Store::viewItems(){
+
 }
 
-struct stationery
-{
-    /* data */
-};
+void Store::editItem(){
+    
+}
+
+void Store::removeItem(){
+    
+}
+
+void Store::searchItem(){
+    
+}
+
 
 int main()
 {
+    Store cart;
+    int choice;
+
+    do
+    {
+        cout<<"=====STATIONERY SHOP MENU====="<<endl;
+        cout<<endl<<"1. Add Item to Cart"<<endl;
+        cout<<"2. View Cart"<<endl;
+        cout<<"3. Edit Cart"<<endl;
+        cout<<"4. Remove Item from Cart"<<endl;
+        cout<<"5. Search Item"<<endl;
+        cout<<"6. Exit"<<endl<<endl;
+
+        cout<<"Enter choice : ";
+        cin>>choice;
+
+        switch (choice)
+        {
+            case 1:
+                cart.addItem();
+                break;
+            case 2:
+                cart.viewItems();
+                break;  
+            case 3:
+                cart.editItem();
+                break;
+            case 4:
+                cart.removeItem();
+                break;
+            case 5:
+                cart.searchItem();
+                break;
+            case 6:
+                cout<<"Program exited.";
+                break;
+            default:
+                cout<<"Invalid choice. Please input a valid choice (1-6)"<<endl;
+                break;
+        }
+
+    } while (choice != 6);   
+
     return 0;
 }
 
