462c64df369e1fcf7069938e3bd240c8b7dce7f4
Quench <clexllama@gmail.com>
2025-11-30 15:11:03 +0800

    Create assignment.cpp

---
+++ assignment.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class store
+{
+private:
+    /* data */
+public:
+    store(/* args */);
+    ~store();
+};
+
+store::store(/* args */)
+{
+}
+
+store::~store()
+{
+}
+
+struct stationery
+{
+    /* data */
+};
+
+int main()
+{
+    return 0;
+}
+
